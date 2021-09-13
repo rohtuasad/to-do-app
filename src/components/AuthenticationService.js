1bf552ff670cb9e61dc20a5e40bfd79019f92f88
Volkov Aleksei <rohtuasad@gmail.com>
2021-09-13 23:52:25 +0300

    Clear saved user from session storage on logout

--- src/components/AuthenticationService.js
+++ src/components/AuthenticationService.js
@@ -2,6 +2,10 @@ class AuthenticationService {
     registerSuccessLogin(username, password) {
         sessionStorage.setItem('authenticatedUser', username)
     }
+
+    logout() {
+        sessionStorage.removeItem('authenticatedUser')
+    }
 }
 
 export default new AuthenticationService();
--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom'
+import AuthenticationService from "./AuthenticationService.js";
 
 class HeaderComponent extends Component {
     render() {
@@ -14,7 +15,7 @@ class HeaderComponent extends Component {
                     </ul>
                     <ul class="navbar-nav navbar-collapse justify-content-end">
                         <li><Link className="nav-link" to="/login">Login</Link></li>
-                        <li><Link className="nav-link" to="/logout">Logout</Link></li>
+                        <li><Link className="nav-link" to="/logout" onClick={AuthenticationService.logout}>Logout</Link></li>
                     </ul>
                 </nav>
             </header>
