314fe76c1f12586b61957c9f6ec1442d6068edeb
Volkov Aleksei <rohtuasad@gmail.com>
2021-09-19 01:17:29 +0300

    Added todo component implementation

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
+import moment from "moment";
+import { Field, Form, Formik } from "formik";
 
 class TodoComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: 1,
+            description: 'Learn forms',
+            targetDate: moment(new Date()).format('YYYY-MM-DD')
+        }
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
     render() {
-        return <div>Todo Component for {this.props.match.params.id}</div>
+        let { description, targetDate } = this.state
+        return (<div>
+            <h1>Todo</h1>
+            <div className='container'>
+                <Formik
+                    initialValues={{ description, targetDate }}
+                    onSubmit={this.onSubmit}
+                >
+                    {
+                        (props) => (
+                            <Form>
+                                <fieldset className='form-group'>
+                                    <label>Description</label>
+                                    <Field className='form-control' type='text' name='description' />
+                                    <label>Target Date</label>
+                                    <Field className='form-control' type='date' name='targetDate' />
+                                </fieldset>
+                                <button className='btn btn-success' type='submit'>Save</button>
+                            </Form>
+                        )
+                    }
+                </Formik>
+
+            </div>
+        </div>)
+    }
+
+    onSubmit(values) {
+        console.log(values)
     }
 }
 
